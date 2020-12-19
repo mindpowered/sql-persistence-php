08c694316922b8e9680dc21eeb5cc43eea8932d2
Build <support@mindpowered.dev>
2020-12-19 17:51:40 +0000

    Version 0.0.9

--- lib/persistence/Persistence.php
+++ lib/persistence/Persistence.php
@@ -2,13 +2,12 @@
 namespace persistence;
 
 use \maglev\MagLev;
-use \maglev\MagLevOld;
 use \maglev\MagLevPhp;
 
 class Persistence
 {
     public function __construct(MagLev $bus) {
-        $this->phpbus = new MagLevPhp(new MagLevOld($bus));
+        $this->phpbus = new MagLevPhp($bus);
         $this->phpbus->register('Persistence.EnglishAuction.Auction.CreateNew', function($args) {
             return "123";
         });
