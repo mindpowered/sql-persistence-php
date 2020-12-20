56c3f2cdf5fa0e3918528f65aed7e016282b3b24
Build <support@mindpowered.dev>
2020-12-20 16:41:59 +0000

    Version 0.0.10

--- lib/persistence/Persistence.php
+++ lib/persistence/Persistence.php
@@ -9,28 +9,35 @@ class Persistence
     public function __construct(MagLev $bus) {
         $this->phpbus = new MagLevPhp($bus);
         $this->phpbus->register('Persistence.EnglishAuction.Auction.CreateNew', function($args) {
-            return "123";
+            $this->noimpl('Persistence.EnglishAuction.Auction.CreateNew');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Auction.FindById', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Auction.FindById');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Auction.FindStarting', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Auction.FindStarting');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Auction.FindEnding', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Auction.FindEnding');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Auction.FindOpen', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Auction.FindOpen');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Bid.CountForAuction', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Bid.CountForAuction');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Bid.FindByHighestPriceForAuction', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Bid.FindByHighestPriceForAuction');
         });
         $this->phpbus->register('Persistence.EnglishAuction.Bid.New', function($args) {
-            return null;
+            $this->noimpl('Persistence.EnglishAuction.Bid.New');
         });
+    }
+
+    private function noimpl(string $method) {
+        $data = ["_not_implemented_", "sql-persistence", "php", $method];
+        $this->phpbus->call("MindPowered.Telemetry.Send", $data);
+        $msg = "You are trying to use the '" . $method . "' method but it's not quite done yet. Please email support@mindpowered.dev to find out when it will be done.";
+        throw new \Exception($msg);
     }
 }
